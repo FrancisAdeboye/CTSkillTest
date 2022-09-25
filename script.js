967ddfb39f1f1a36e4820d0d4e61261e5fd630be
Francis Adeboye <francisadeboye24@gmail.com>
2022-09-25 14:42:09 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function background1() {
-    console.log("clicked")
     var imageUrl = "images/mountain1.png";
     $(".body-3").css("background-image", "url(" + imageUrl + ")");
+    $(".mountain-1").css("text-decoration", "underline");
 }
 function background2() {
-    console.log("clicked2")
     var imageUrl2 = "images/mountain2.png";
     $(".body-3").css("background-image", "url(" + imageUrl2 + ")");
+    $(".mountain-2").css("text-decoration", "underline");
 }
